6a30aeffeaefa820cd593e637b5cee8300c10cac
pneuly <pneuly@gmail.com>
2024-12-13 10:35:12 +0900

    workflow test

---
+++ src/sample.py
@@ -0,0 +1,10 @@
+import sys
+
+def print_path():
+    print('\n'.join(sys.path))
+
+def add(a, b):
+    return a + b
+
+def subtract(a, b):
+    return a - b
---
+++ tests/test_try.py
@@ -0,0 +1,9 @@
+from src.sample import add, subtract, print_path
+
+def test_add(sample_numbers):
+    result = add(sample_numbers["a"], sample_numbers["b"])
+    assert result == 15
+
+def test_subtract(sample_numbers):
+    result = subtract(sample_numbers["a"], sample_numbers["b"])
+    assert result == 5
