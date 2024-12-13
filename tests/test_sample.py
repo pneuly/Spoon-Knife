b526f8004f8491f34a9d3bf3532ea87d6c11bc70
pneuly <pneuly@gmail.com>
2024-12-13 11:54:16 +0900

    test

--- tests/test_sample.py
+++ tests/test_sample.py
@@ -1,6 +1,7 @@
 from src.sample import add, subtract, print_path
 
 def test_add(sample_numbers):
+    print_path()
     result = add(sample_numbers["a"], sample_numbers["b"])
     assert result == 15
 
